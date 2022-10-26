501e6f97dc77dbb97f0a9deb2f52b2e686341bf1
Serkan Korkut <serkan.korkut@thundra.io>
2022-10-26 15:34:54 +0300

    message2

--- create_prs.sh
+++ create_prs.sh
@@ -4,6 +4,7 @@ do
    git checkout master
    git checkout -b "branch$i"
    touch "$i"
+   git add .
    git commit -am "Message$i"
    git push origin "branch$i"
   gh pr create --title "The bug is fixed $i"  --body "Everything works fine$i"
